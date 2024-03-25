5538eb7571362bd993c7ee75647bdbeffd5fbdbd
Student <Student@703-110>
2024-03-25 10:12:08 +0300

    234

--- 3.2_/3.2(bit).cpp
+++ 3.2_/3.2(bit).cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 void SplitNumbers(const string& infilename, const string& outfilename)
 {
-    ifstream infile(infilename,ios::binary);
+    fstream infile(infilename, ios::binary |ios::in|ios::out);
     ofstream outfile(outfilename ,ios::binary);
     outfile.clear();
     int num;
+
+    int data[] = { 1, 2, 3, 4, 5 ,6 ,7 ,8 , 1 , 2 ,3};
+    infile.write(reinterpret_cast<const char*>(&data), sizeof(data));
+    infile.clear();
+
+    infile.seekg(0, ios::beg);
     while (infile.read(reinterpret_cast<char*>(&num), sizeof(num)))
     {
         if (num % 2 == 0) outfile.write(reinterpret_cast<char*>(&num), sizeof(num));
